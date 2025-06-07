965fbea4759b4b00a5d48b462f24303b62a70354
MrZ_26 <1046101471@qq.com>
2025-06-07 15:31:50 +0800

    first commit

---
+++ chordGen.lua
@@ -0,0 +1,346 @@
+local dimData = {
+    [0] = { -- 0D
+        yStep = 0,
+        draw = 'none',
+    },
+    { -- 1D (octave)
+        yStep = math.log(8 / 4, 2),
+        draw = 'arrow',
+        color = '808080',
+    },
+    { -- 2D (fifth)
+        yStep = math.log(6 / 4, 2),
+        draw = 'left',
+        color = 'F27A93',
+    },
+    { -- 3D (third)
+        yStep = math.log(5 / 4, 2),
+        draw = 'right',
+        color = '6DD884',
+    },
+    { -- 4D
+        yStep = math.log(7 / 4, 2),
+        draw = 'rise',
+        color = 'B498EE',
+    },
+    { -- 5D
+        yStep = math.log(11 / 4, 2),
+        draw = 'fall',
+        color = 'FFC247',
+    },
+    { -- 6D
+        yStep = math.log(13 / 4, 2),
+        draw = 'arcleft',
+        color = 'B5B539',
+    },
+    { -- 7D
+        yStep = math.log(17 / 4, 2),
+        draw = 'arcright',
+        color = 'E19C7D',
+    },
+}
+for i = 1, #dimData do
+    local dim = dimData[i]
+    dimData[-i] = {
+        yStep = -dim.yStep,
+        draw = dim.draw,
+        color = dim.color,
+    }
+end
+
+---@alias SsvtDim number
+
+---@class SsvtChord
+---@field d? SsvtDim
+---@field note? 'skip' | 'dotted'
+---@field bias? 'l' | 'r'
+---@field [number] SsvtChord
+
+local lw = .1
+local nw = .014
+
+local drawBuffer
+
+local ucs_x, ucs_y = 0, 0
+local function moveOrigin(dx, dy)
+    ucs_x = ucs_x + dx
+    ucs_y = ucs_y + dy
+end
+
+local function polygon(layer, color, ...)
+    local points = { ... }
+    for i = 1, #points, 2 do
+        points[i] = points[i] + ucs_x
+        points[i + 1] = points[i + 1] + ucs_y
+    end
+    table.insert(drawBuffer, {
+        layer = layer,
+        color = color,
+        points = points,
+    })
+end
+
+local function lerp(a, b, t)
+    return a * (1 - t) + b * t
+end
+
+local function drawNote(mode, x1, x2, y)
+    if mode == 'dotted' then
+        -- Dotted line
+        for i = 0, 10, 2 do
+            local x = lerp(x1 + .05, x2 - .05, i / 11)
+            local w = (x2 - x1 - .1) / 11
+            polygon(0, "F0F0F0",
+                x, y - nw / 2,
+                x + w, y - nw / 2,
+                x + w, y + nw / 2,
+                x, y + nw / 2
+            )
+        end
+    elseif mode == 'skip' then
+        -- Short line
+        x1, x2 = lerp(x1, x2, .3), lerp(x2, x1, .3)
+        polygon(0, "808080",
+            x1 + .05, y - nw / 2,
+            x2 - .05, y - nw / 2,
+            x2 - .05, y + nw / 2,
+            x1 + .05, y + nw / 2
+        )
+    else
+        -- Line
+        polygon(0, "F0F0F0",
+            x1 + .05, y - nw / 2,
+            x2 - .05, y - nw / 2,
+            x2 - .05, y + nw / 2,
+            x1 + .05, y + nw / 2
+        )
+    end
+end
+
+local function drawBeam(color, mode, x1, y1, x2, y2)
+    if mode == 'none' then return end
+    if mode == 'arrow' then
+        local m = (x1 + x2) / 2
+        polygon(1, color,
+            m, y2,
+            m + lw * .8, y2 * .9 + y1 * .1,
+            m + lw * .2, y2 * .9 + y1 * .1,
+            m + lw * .2, y1,
+            m - lw * .2, y1,
+            m - lw * .2, y2 * .9 + y1 * .1,
+            m - lw * .8, y2 * .9 + y1 * .1
+        )
+    else
+        if y1 > y2 then y1, y2 = y2, y1 end
+        y1, y2 = y1 - nw / 2, y2 + nw / 2
+        if mode == 'left' then
+            polygon(2, color,
+                x1, y1,
+                x1 + lw, y1,
+                x1 + lw, y2,
+                x1, y2
+            )
+        elseif mode == 'right' then
+            polygon(2, color,
+                x2, y1,
+                x2 - lw, y1,
+                x2 - lw, y2,
+                x2, y2
+            )
+        elseif mode == 'rise' then
+            polygon(3, color,
+                x1, y1,
+                x1 + lw * 1.26, y1,
+                x2, y2,
+                x2 - lw * 1.26, y2
+            )
+        elseif mode == 'fall' then
+            polygon(3, color,
+                x2, y1,
+                x2 - lw * 1.1, y1,
+                x1, y2,
+                x1 + lw * 1.1, y2
+            )
+        elseif mode == 'mid' then
+            local m = (x1 + x2) / 2
+            polygon(2, color,
+                m - lw / 4, y1,
+                m + lw / 4, y1,
+                m + lw / 4, y2,
+                m - lw / 4, y2
+            )
+        elseif mode == 'halfleft' then
+            local m = (x1 + x2) / 3
+            polygon(4, color,
+                m - lw / 2, y1,
+                m + lw / 2, y1,
+                m + lw / 2, y2,
+                m - lw / 2, y2
+            )
+        else
+            error("Unknown beam style: " .. mode)
+        end
+    end
+end
+
+---@param chord SsvtChord
+---@param x1 number
+---@param x2 number
+function DrawBranches(chord, x1, x2)
+    for n = 1, #chord do
+        local nxt = chord[n]
+        if nxt.bias == 'l' then
+            DrawBranch(nxt, x1, x2 - .16)
+        elseif nxt.bias == 'r' then
+            DrawBranch(nxt, x1 + .16, x2)
+        else
+            DrawBranch(nxt, x1, x2)
+        end
+    end
+end
+
+---@param chord SsvtChord
+---@param x1 number
+---@param x2 number
+function DrawBranch(chord, x1, x2)
+    local nData = dimData[chord.d]
+
+    -- Note
+    drawNote(chord.note, x1, x2, nData.yStep)
+
+    -- Beam
+    drawBeam(nData.color, nData.draw, x1, 0, x2, nData.yStep)
+
+    -- Branches
+    moveOrigin(0, nData.yStep)
+    DrawBranches(chord, x1, x2)
+    moveOrigin(0, -nData.yStep)
+end
+
+---@param chord SsvtChord
+function DrawChord(chord)
+    drawBuffer = {}
+    drawNote(chord.note, 0, 1, 0)
+    DrawBranches(chord, 0, 1)
+    table.sort(drawBuffer, function(a, b) return a.layer < b.layer end)
+    return drawBuffer
+end
+
+---@param dat string
+---@return SsvtChord
+local function SsvtDecode(dat)
+    ---@type SsvtChord
+    local buf = { d = 0 }
+    local note = dat:match("^%-?%d+")
+    if note then
+        buf.d = tonumber(note:match("%-?%d+"))
+        dat = dat:sub(#note + 1)
+    end
+    while true do
+        local char = dat:sub(1, 1)
+        if char == '.' then
+            if math.abs(buf.d) == 1 then
+                buf.note = 'skip'
+            else
+                buf.note = 'dotted'
+            end
+        elseif char == 'l' then
+            buf.bias = 'l'
+        elseif char == 'r' then
+            buf.bias = 'r'
+        else
+            break
+        end
+        dat = dat:sub(2)
+    end
+    local branch = string.match(dat, "%b()")
+    if branch then
+        branch = branch:sub(2, -2) -- Remove outer parentheses (and garbages come after)
+        local resStrings = {}
+        local balance = 0
+        local start = 1
+        for i = 1, #branch do
+            local char = branch:sub(i, i)
+            if char == "(" then
+                balance = balance + 1
+            elseif char == ")" then
+                balance = balance - 1
+                assert(balance >= 0, "More ( than )")
+            elseif char == "," and balance == 0 then
+                table.insert(resStrings, branch:sub(start, i - 1))
+                start = i + 1
+            end
+        end
+        table.insert(resStrings, branch:sub(start))
+        for i = 1, #resStrings do
+            table.insert(buf, SsvtDecode(resStrings[i]))
+        end
+    end
+    return buf
+end
+
+local svgPattern = [[<svg width="$W" height="$H" viewBox="0 0 $w $h" xmlns="http://www.w3.org/2000/svg">$SHAPES</svg>]]
+-- <rect x="1" y="1" width="1" height="1" fill="#FFB900" />
+local function toSvg(data)
+    -- Calculate bounding box
+    local minX, maxX, minY, maxY = 999, -999, 999, -999
+    for i = 1, #data do
+        local shape = data[i].points
+        for j = 1, #shape, 2 do
+            local x, y = shape[j], shape[j + 1]
+            if x < minX then minX = x elseif x > maxX then maxX = x end
+            if y < minY then minY = y elseif y > maxY then maxY = y end
+        end
+    end
+
+    -- Snap to zero & Flip vertically
+    maxX, maxY = maxX - minX, maxY - minY
+    for i = 1, #data do
+        local shape = data[i].points
+        for j = 1, #shape, 2 do
+            shape[j] = shape[j] - minX
+            shape[j + 1] = maxY - (shape[j + 1] - minY)
+        end
+    end
+
+    -- Strinify
+    for i = 1, #data do
+        local shape = data[i].points
+        for j = 1, #shape do
+            shape[j] = string.format("%.4g", shape[j])
+        end
+    end
+
+    local scale = math.max(256 / maxX, 256 / maxY)
+    local res = svgPattern
+    res = res:gsub("$W", tostring(scale * maxX), 1)
+    res = res:gsub("$H", tostring(scale * maxY), 1)
+    res = res:gsub("$w", tostring(maxX), 1)
+    res = res:gsub("$h", tostring(maxY), 1)
+
+    local shapeData = ""
+    for i = 1, #data do
+        local shape = data[i]
+        shapeData = shapeData ..
+            ([['<polygon points="%s" fill="#%s" />']]):format(
+                table.concat(shape.points, ","),
+                shape.color
+            )
+    end
+    res = res:gsub("$SHAPES", shapeData)
+
+    return res
+end
+
+if #arg == 0 then
+    print("Usage: lua chord.lua <chord1> <chord2> ...")
+else
+    for i = 1, #arg do
+        local chordStr = arg[i]
+        local chord = SsvtDecode(chordStr)
+        local svgData = toSvg(DrawChord(chord))
+        local fileName = "output_" .. i .. ".svg"
+        io.open(fileName, "w"):write(svgData):close()
+    end
+    print("SVG files generated successfully.")
+end
---
+++ readme.md
@@ -0,0 +1,83 @@
+# SVG Shasavistic Chord Graph Generator
+
+|                                         |                                         |
+| :-------------------------------------: | :-------------------------------------: |
+| ![example_output1](/image/output_1.svg) | ![example_output2](/image/output_2.svg) |
+|              (2(3r),3r(2))              |             (2.(3(-4r(3))))             |
+
+![example_image](/image/chords_example.png)
+
+> Screenshot taken from [this video](https://youtu.be/8nxWoh4NBeE) (youtube)
+
+Environment: Lua
+
+Usage: `lua chordGen.lua "[chord1]" "[chord2]" ...`
+
+SVG files will be created at current directory.
+
+## Syntax
+
+`0/1/2/3/4/5/6`: note
+
+0`(...)`: note groups (separated by comma)
+
+`-`0: move down
+
+0`r`: put at right side
+
+0`l`: put at left side
+
+0`.`: skipped note
+
+0`x`: explicitly specify the root note (not implemented yet)
+
+## Other Examples
+
+| Chord   | Name               |
+| ------- | ------------------ |
+| (?)Maj  | `(2,3r)`           |
+| (?)Min  | `(2(-3r))`         |
+| (?)Maj7 | `(2(3r),3r(2))`    |
+| (?)Min7 | `(2(-3r(2)))`      |
+| (?)7    | `(2,3r,-2.(-2))`   |
+| (?)dim  | `.(2,3xr,-2.(-2))` |
+| (?)aug  | `(3(3))`           |
+| (?)sus2 | `(2(2))`           |
+| (?)sus4 | `(2x(2))`          |
+| (?)add4 | `(2(2),3r)`        |
+
+## Tutorial
+
+The basic unit looks like:
+
+`0(...)`
+
+where `0` is the movement, and `(...)` contains more notes with same format based on it.
+
+Major chord example:
+
+It's composed of three notes:  
+Root note `0`  
+Root note with upward `2D movement`  
+Root note with upward `3D movement`
+
+So the chord looks like:
+
+`0(2,3)`
+
+Where `0` is the root note, and `2` and `3` are the notes based on it.
+
+First number `0` can be omitted, so final result is:
+
+`(2,3)`
+
+We can notice that note `3` overlaps with the note `2`, so we move it to `R`ight side:
+
+`(2,3r)`
+
+Minor chord example:
+
+`(2(-3))`
+
+Note `3` is based on note `2`, so it's put in parentheses after `2`.  
+And it goes down, so there's a minus sign before it.
